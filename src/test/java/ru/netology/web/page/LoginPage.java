c96a21246e07047798d7962e2b7b3a0fc742a44c
Dmitrii Odynets <158339965+dimosaintp@users.noreply.github.com>
2024-08-05 21:56:12 +0300

    Added classes date and page

---
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -0,0 +1,19 @@
+package ru.netology.web.page;
+
+import static com.codeborne.selenide.Selenide.$;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+public class LoginPage {
+
+    private final SelenideElement loginField = $("[data-test-id=login] input");
+    private final SelenideElement passwordField = $("[data-test-id=password] input");
+    private final SelenideElement loginButton = $("[data-test-id=action-login]");
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+}
